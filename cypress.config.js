8b50ff7dd92bf3bd8247221289152b62cab0570e
Vinayak Mahajan <mahajavi@amazon.com>
2024-08-13 12:24:13 +0100

    Renamed cypress config file from .ts to .js

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,4 @@
-import {defineConfig} from "cypress";
+const { defineConfig } = require("cypress")
 const cucumber = require('cypress-cucumber-preprocessor').default
 
 module.exports = defineConfig({
